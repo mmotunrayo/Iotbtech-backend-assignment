9967f5430522d0ee263e84c3c5f38a4807a35477
mmotunrayo <mmotunrayo@users.noreply.github.com>
2026-09-19 13:38:26 +0100

    add some changes

--- src/phase-b/index.ts
+++
@@ -1,2 +0,0 @@
-﻿export * from './types/reading.types';
-export * as ReadingService from './services/reading.service';
--- src/phase-c/index.ts
+++
@@ -1,5 +0,0 @@
-﻿export { requestLogger } from './middleware/requestLogger';
-export { requireApiKey } from './middleware/requireApiKey';
-export { notFoundHandler } from './middleware/notFoundHandler';
-export { errorHandler } from './middleware/errorHandler';
-export type { AppError } from './middleware/errorHandler';
